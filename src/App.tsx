a5c4cbb6ab5f0a5d92c79cfb0b5d6a2cb59dd323
ritesh <riteshsolke12@gmail.com>
2025-08-03 01:04:28 +0530

    added some changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Hero } from "./components/Hero";
+import { Footer } from "./components/Footer";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -17,6 +20,10 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/hero" element={<Hero />} />
+          <Route path="/footer" element={<Footer/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
